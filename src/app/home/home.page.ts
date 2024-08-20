b4bf6ee47d1a43409012fd12d1946a682a660269
julioonmartinezal200 <julioonmartinez@gmail.com>
2024-08-19 22:24:14 -0600

    Primera versión del proyecto Amara

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule]
+})
+export class HomePage {
+  constructor() {}
+}
